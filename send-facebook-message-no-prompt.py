fa8e8257fc377fa8bb80f6d9053338658a020517
Cdoherty95 <cdoherty@hawk.iit.edu>
2019-01-22 16:43:07 -0600

    Added comments to py file

--- send-facebook-message-no-prompt.py
+++ send-facebook-message-no-prompt.py
@@ -1,21 +1,18 @@
-"""
-
-pyinstaller --onefile --add-data "messages.txt;messages.txt" --add-data "messages.bak;messages.bak" --add-data "creds.config;creds.config" fbchat_send_message.py
-Gives me the error no messages in file
-
-"""
 import os
 import random
 import sys
+
 from fbchat import Client
 from fbchat.models import *
 
+# File Names in the data folder. they will be joined with the resource_path function
 messages_file_name = "messages.txt"
 messages_backup_file_name = "messages.bak"
 credentials_file_name = "creds.config"
 data_dir = "data"
 
 
+# Used to ensure we have the correct path after building exe
 def resource_path(relative):
     if hasattr(sys, "_MEIPASS"):
         return os.path.join(sys._MEIPASS, relative)
@@ -23,11 +20,14 @@ def resource_path(relative):
     return os.path.join(relative)
 
 
+# Variables for file references
 messages_file = resource_path(os.path.join(data_dir, messages_file_name))
 messages_backup_file = resource_path(os.path.join(data_dir, messages_backup_file_name))
 credentials_file = resource_path(os.path.join(data_dir, credentials_file_name))
 
 
+# Creates the client connection
+# Returns the connection to pass into the send_message and the username we want to send to
 def create_client():
     credentials = {}
     with open(credentials_file) as f:
@@ -40,6 +40,7 @@ def create_client():
     return client_connection, credentials['user_name']
 
 
+# finds the user id numbers based on the username we want to send to
 def getuserid(client_connection, username):
     # `searchForUsers` searches for the user and gives us a list of the results,
     # and then we just take the first one, aka. the most likely one:
@@ -48,18 +49,24 @@ def getuserid(client_connection, username):
     return user.uid
 
 
+# function pulls a message from the messages.txt file and returns it to be sent
 def find_a_Message():
     all_messages = []
 
     # Read in all lines from the file storing as list
     try:
+        # Open the message.txt file and read in all the content
         with open(messages_file, "r", encoding="utf8") as f:
             all_messages = f.readlines()
         f.close()
 
+        # Find the number of messages that were read in
         len_of_list = len(all_messages)
 
-        # If there are no messages copy all from backup file
+        # If there are no messages read in from messages.txt then copy all
+        # the messages from the backup file to the messages.txt file
+        # This ensures we use all the messages only once then will reuse messages
+        # should be long enough period to forget we already sent that message
         if len_of_list == 0:
             with open(messages_backup_file, "r", encoding="utf8") as f:
                 all_messages = f.readlines()
@@ -70,18 +77,22 @@ def find_a_Message():
                 fw.write(all_messages[x])
             fw.close()
 
-        # generate random number to use as index for message
+        # generate random number to use as index for message to select
+        # this ensures we do not go in order
         message_index_to_send = random.randint(0, (len_of_list - 1))
 
-        # Open messages.text to write back all messages we didn't use
+        # Open messages.txt to write back all messages we didn't use
         fw = open(messages_file, "w", encoding="utf8")
         for x in range(len_of_list):
+            # will not write the message at the random index to ensure we only send it once
             if x != message_index_to_send:
                 fw.write(all_messages[x])
         fw.close()
 
+        # select the message based on the random index we will send
         message_to_send = all_messages[message_index_to_send]
 
+        # Return the message string
         return message_to_send
 
     # If there are no more messages to send
@@ -90,30 +101,35 @@ def find_a_Message():
         sys.exit(-1)
 
 
+# this will send the message to the user we wanted
 def send_message(client_connection, cust_message, thread_id):
+    # Thread type is use to ensure the client knows it is a user we are sending to not a group
     thread_type = ThreadType.USER
+
     # Will send a message to the thread
     try:
         output = client_connection.send(Message(text=cust_message), thread_id=thread_id, thread_type=thread_type)
         return output
+
+    # likely error is fb locked the account due to suspicious activity
     except Exception as error:
         print("Failed Sending Message: " + str(error))
         sys.exit(-1)
 
 
 def main():
-    # S3: compose message - moved to top so we do not have to keep trying if there
-    # are no more messages
+    # S1: Fina a message from the messages.txt file we will send to the desired user
     message = find_a_Message()
 
-    # step 1: login
+    # Step 2: create a connection to facebook and save return to variables
     client_connection, user_name = create_client()
 
-    # s2: get user ID from user name
+    # Step 3: get the user ID from user name
     uid = getuserid(client_connection, user_name)
 
-    # s4: send message
+    # Step 4: send the message from the file to the user
     send_message(client_connection, message, uid)
 
 
+# Calls the main function
 main()
